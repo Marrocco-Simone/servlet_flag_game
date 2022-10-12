9bbb44b008b885b7150c82ff171fb32a53ac3ffc
simone_virtualbox <simone.marrocco@studenti.unitn.it>
2022-10-12 19:21:24 +0200

    game works

--- src/main/java/it/unitn/marrocco/flaggame/Game.java
+++ src/main/java/it/unitn/marrocco/flaggame/Game.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
@@ -37,7 +38,7 @@ public class Game extends HttpServlet {
             out.println("<th>Index</th>");
             out.println("<th>Capital</th>");
         out.println("</tr>");
-        int rank = 1;
+        int rank = 0;
         for(String capital: capitals) {
             out.println("<tr>");
             out.println("<td>"+rank+"</td>");
@@ -64,16 +65,24 @@ public class Game extends HttpServlet {
         return chosen_capitals;
     }
 
-    public void printChosenCapitals(PrintWriter out, ArrayList<String> chosen_capitals) {
-        int counter = 0;
-        for(String capital: chosen_capitals){
-            out.println("<div>");
-            out.println("<img src='flags/"+capital+".png' width='150' height='100'></img>");
-            out.println("<label>Guess<input name='capital_"+counter+"' type='number'/></label>");
-            out.println("</div>");
-
-            counter++;
+    public int findCapitalId(String capital, ArrayList<String> capitals) {
+        for(int i=0; i<capitals.size(); i++){
+            if(capital.equals(capitals.get(i))) return i;
         }
+        return -1;
+    }
+
+    public void printChosenCapitals(PrintWriter out, ArrayList<String> capitals, ArrayList<String> chosen_capitals) {
+        out.println("<form action='game' method='POST'>");
+        int max = capitals.size() -1;
+        for(String capital: chosen_capitals){
+            out.println("<div><label>");
+            out.println("<img src='flags/"+capital+".png' width='150' height='100' />");
+            out.println("<input name='"+findCapitalId(capital, capitals)+"' type='number' required min='0' max='"+max+"'/>");
+            out.println("</label></div>");
+        }
+        out.println("<button type='submit'>Submit Responses</button>");
+        out.println("</form>");
     }
 
     @Override
@@ -89,8 +98,34 @@ public class Game extends HttpServlet {
         printCapitalsList(out, capitals);
 
         ArrayList<String> chosen_capitals = getChosenCapitals(capitals);
-        printChosenCapitals(out, chosen_capitals);
+        printChosenCapitals(out, capitals, chosen_capitals);
         Main.addHtmlFragment(req, res, "fragments/html_file_end.html");
         out.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        UserSession user = Main.getUserSession(req, res);
+        if (user == null) return;
+
+        Enumeration<String> param_names = req.getParameterNames();
+        boolean guessed_all = true;
+        while(param_names.hasMoreElements()){
+            String param_name = param_names.nextElement();
+            String param_value = req.getParameter(param_name);
+            if(!param_value.equals(param_name)){
+                guessed_all = false;
+                break;
+            }
+        }
+        if(guessed_all) {
+            user.points += 3;
+        }else {
+            if(user.points > 0) user.points -= 1;
+        }
+
+        HttpSession session = req.getSession();
+        session.setAttribute("points", user.points);
+        res.sendRedirect("index.html");
     }
 }
